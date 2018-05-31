dda04edba1d06e3718efed16a1578111efba5d8f
gabrielifeliz <gfeliz3@montgomerycollege.edu>
2018-05-31 14:55:03 -0400

    initial commit

---
+++ src/com/company/MathTwo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MathTwo {
+
+    public static void main(String[] args) {
+        int myNumber = 2;
+        double myOtherNumber = 1.7938;
+
+        System.out.println(myNumber + "\n" + myOtherNumber);
+    }
+}
